475f97b398fdd10f32e7c1512d475c68f453eb4e
Mattlecodeurdelextrem <Maty.Gentili@gmail.com>
2022-04-30 18:49:21 +0200

    calendar_coursv3

--- ESME_eleve/app/src/main/java/com/example/esme/CalendarActivity.java
+++ ESME_eleve/app/src/main/java/com/example/esme/CalendarActivity.java
@@ -1,21 +1,17 @@
 package com.example.esme;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
--- ESME_eleve/app/src/main/java/com/example/esme/New_Event_Cours.java
+++ ESME_eleve/app/src/main/java/com/example/esme/New_Event_Cours.java
@@ -7,6 +7,7 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
@@ -16,8 +17,10 @@ import java.util.Locale;
 public class New_Event_Cours extends Activity {
 
     Button TimeButtonD,TimeButtonF,DateButton;
+    CheckBox chk;
     int hour,minute;
     private DatePickerDialog datePickerDialog;
+    private String intitule,choix_date,heure_d,heure_f,importance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +30,45 @@ public class New_Event_Cours extends Activity {
         TimeButtonD= findViewById(R.id.select_Hour_Debut_Event);
         TimeButtonF= findViewById(R.id.select_Hour_Fin_Event);
         DateButton= findViewById(R.id.Select_Date_Event);
+        chk = (CheckBox) findViewById(R.id.checkBox_Importance);
 
     }
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        if (checked) {
+            importance="Important";
+                }
+        else{
+            importance="Non Important";
+                }
+    };
 
 
+    private void popDatePicker() {
+
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                String date = String.format("%02d",(day))+"/"+String.format("%02d",(month+1))+"/"+year;
+                DateButton.setText(date);
+                choix_date=date;
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+        datePickerDialog= new DatePickerDialog(this, style, dateSetListener, year, month, day );
+    }
+
+    public void popDatePicker(View view) {
+
+        datePickerDialog.show();
+
+    }
 
     public void popTimePickerDebut(View view) {
 
@@ -39,16 +77,13 @@ public class New_Event_Cours extends Activity {
             public void onTimeSet(TimePicker timePicker, int selectHour, int selectMin) {
                 hour = selectHour;
                 minute =selectMin;
-
                 TimeButtonD.setText(String.format(Locale.getDefault(),"%02d:%02d",hour,minute));
-
+                heure_d=String.format(Locale.getDefault(),"%02d:%02d",hour,minute);
             }
         };
 
         int style= AlertDialog.THEME_HOLO_DARK;
-
         TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener,hour,minute,true);
-
         timePickerDialog.setTitle("Heure Debut");
         timePickerDialog.show();
     }
@@ -60,44 +95,15 @@ public class New_Event_Cours extends Activity {
             public void onTimeSet(TimePicker timePicker, int selectHour, int selectMin) {
                 hour = selectHour;
                 minute =selectMin;
-
                 TimeButtonF.setText(String.format(Locale.getDefault(),"%02d:%02d",hour,minute));
-
+                heure_f=String.format(Locale.getDefault(),"%02d:%02d",hour,minute);
             }
         };
 
         int style= AlertDialog.THEME_HOLO_DARK;
-
         TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener,hour,minute,true);
-
         timePickerDialog.setTitle("Heure Fin");
         timePickerDialog.show();
     }
 
-    private void popDatePicker() {
-
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-
-                String date = String.format("%02d",(day))+"/"+String.format("%02d",(month+1))+"/"+year;
-                DateButton.setText(date);
-            }
-        };
-
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-
-        int style = AlertDialog.THEME_HOLO_LIGHT;
-
-        datePickerDialog= new DatePickerDialog(this, style, dateSetListener, year, month, day );
-    }
-
-    public void popDatePicker(View view) {
-
-    datePickerDialog.show();
-
-    }
 }
